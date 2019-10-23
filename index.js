f6c3d2ce77d6c933c1ad0686a92d52ba798788bb
Fah11ad <falqurashi11@gmail.com>
2019-10-23 12:46:22 +0300

    upload nmp package

---
+++ index.js
@@ -0,0 +1,16 @@
+let greeting = "Hi suhaib"
+let question = "how was today class ?"
+let test = () => {
+    let x = 20
+    while(x>20){
+        console.log(`Today is sooo annoying class!`);
+        x++
+        
+    }
+}
+
+module.exports = {
+    greeting,
+    question,
+    test
+}
